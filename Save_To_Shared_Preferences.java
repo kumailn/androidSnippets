c3ac5940c3ff237e9314db462e8ab9e9b61006f5
Kumail <kumailmn@gmail.com>
2017-08-26 14:02:18 -0400

    -
    Updated info

---
+++ Save_To_Shared_Preferences.java
@@ -0,0 +1,10 @@
+//Save data via SharedPreferences
+
+public void save(String myString){
+    //Creates xml file in local directory containging key-value pairs
+    SharedPreferences sharedPreferences = getSharedPreferences("myData", Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = sharedPreferences.edit();
+    // Key name = Item1, Value = myString
+    editor.putString("Item1", myString);
+    editor.commit();
+}
