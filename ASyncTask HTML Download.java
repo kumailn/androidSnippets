87ed9d05b724287a5019f3345ed73c00f2647e35
Kumail <kumailmn@gmail.com>
2017-10-10 01:19:10 -0400

    added comments

--- ASyncTask HTML Download.java
+++ ASyncTask HTML Download.java
@@ -1,26 +1,37 @@
-public class DownloadTask extends AsyncTask<String , Void, String>{
+//AsyncTask class - String (input type), Void (Action), String (return type)
+    public class DownloadTask extends AsyncTask<String , Void, String>{
         @Override
+        //main method
         protected String doInBackground(String... strings) {
-            Log.e("Download form URL: ", strings[0]);
+            Log.e("Download from URL: ", strings[0]);
             String result = "";
             URL url;
+            //new HTTP connection
             HttpURLConnection urlConnection = null;
             try{
+                //create URL type from input string
                 url = new URL(strings[0]);
+                //Open URL connection with given URL
                 urlConnection = (HttpURLConnection) url.openConnection();
+                //Create input stream
                 InputStream in = urlConnection.getInputStream();
+                //Create input stream reader
                 InputStreamReader reader = new InputStreamReader(in);
                 int data = reader.read();
+                //last element of data == -1
                 while(data != -1){
+                    //cast data to char
                     char current = (char)(data);
+                    //add char to result string
                     result += current;
+                    //move onto next character
                     data = reader.read();
                 }
             }
             catch(Exception e){
                 e.printStackTrace();
             }
-            //Result is a string of the websites complete HTML data
+            //Return string of HTML data
             return result;
 
         }
