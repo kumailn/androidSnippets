03607ff120e6a652e1482bef3b5784bb1e19bb57
Kumail <kumailmn@gmail.com>
2017-10-10 22:26:08 -0400

    Updated Comments

--- ASyncTask Bitmap Download.java
+++ ASyncTask Bitmap Download.java
@@ -1,21 +1,26 @@
+//AsyncTask class - String (input type), Void (Action), String (return type)
 public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
 
         @Override
         protected Bitmap doInBackground(String... strings) {
             try {
+              //New url from string
                 URL url = new URL(strings[0]);
+                //Open HTTP connection to url
                 HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                //Connect
                 httpURLConnection.connect();
+                //Create inputStream
                 InputStream inputStream = httpURLConnection.getInputStream();
+                //Convert input stream to bitmap with BitmapFactory
                 Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                //Return resulting bitmap
                 return  bitmap;
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
             return null;
         }
     }
