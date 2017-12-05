bb8aae4e657e8b4e308b232a9e70201199c3db96
Kumail Naqvi <kumailmn@gmail.com>
2017-12-05 14:18:46 -0500

    fix

--- SQL_Example.java
+++ SQL_Example.java
@@ -1,8 +1,8 @@
 //Create SQL database
 SQLiteDatabase database = openOrCreateDatabase("databaseName", MODE_PRIVATE, null);
 //Execute SQL statement on database
-database.execSQL("CREATE TABLE IF NOT EXISTS tableName(fieldOne VARCHAT, field2 INT)");
+database.execSQL("CREATE TABLE IF NOT EXISTS tableName(fieldOne VARCHAR, field2 INT)");
 
-//Use a cursor object to return a value 
+//Use a cursor object to return a value
 Cursor c = database.rawQuery("SELECT something FROM tableName", null);
 c.moveToFirst();
