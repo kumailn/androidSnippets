4ce4f5bf3f85a461d1243b17d424093cf4d519c1
Kumail Naqvi <kumailmn@gmail.com>
2017-12-16 22:47:59 -0500

    +

--- Misc.java
+++ Misc.java
@@ -12,3 +12,9 @@ String myString = "hello this is a test";
         while(matcher.find()){
             System.out.println(matcher.group(1));
         }
+
+
+//Convert milliseconds to database
+SimpleDateFormat sdf = new SimpleDateFormat("MMM dd/yyyy hh:mm");
+Date date = new Date(System.currentTimeMillis());
+String dateString = sdf.format(date);
